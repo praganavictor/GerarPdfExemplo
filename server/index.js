6f69bac004b54a39ecc229e3beb691c127c74671
Victor Pragana <victormerigue@gmail.com>
2019-07-03 17:00:29 -0300

    enlouquecendo mesmo que eu não queira

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const bodyparser = require("body-parser");
-const pdf = require("html-pdf");
 const cors = require("cors");
 
-const pdfTemplate = require("./documents");
+const pdfTemplate = require("./documents/novo");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,11 +11,13 @@ app.use(cors());
 app.use(express.json());
 
 app.post("/create-pdf", async (req, res) => {
-  await pdfTemplate(req.body).then(res.send(true));
+  await pdfTemplate(req.body)
+    .then(res.send(true))
+    .catch(err => console.log(err));
 });
 
 app.get("/fetch-pdf", (req, res) => {
-  res.sendFile(`${__dirname}/output.pdf`);
+  res.sendFile(`${__dirname}/novopdf.pdf`);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
